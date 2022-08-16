61adeedcf1854cc3088cead102c4426e531c2283
Damian Wdowiak <damian.wdowiak@htdevelopers.com>
2022-08-16 20:41:07 +0200

    handle connection lost

---
+++ src/pages/game-pages/CheckersPage/constants.tsx
@@ -0,0 +1,9 @@
+const GAME_STATUS = {
+    wait: 'wait',
+    on: 'on',
+    draw: 'draw',
+    won: 'won',
+    lost: 'lost',
+};
+
+export { GAME_STATUS };
--- src/pages/game-pages/CheckersPage/index.tsx
+++ src/pages/game-pages/CheckersPage/index.tsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { ungzip, gzip } from 'pako';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCrown } from '@fortawesome/free-solid-svg-icons';
 
 import decodeUtf8 from 'global/util/decodeUtf8';
+import encodeUtf8 from 'global/util/encodeUtf8';
 
 import { Container, Board, Field, Piece, GameStatus } from './styled';
 import { CheckersPageProps, GameState } from './types';
 import { INITIAL_STATE } from './helpers';
+import { GAME_STATUS } from './constants';
 
 const HEADER = 'Checkers by CheretKa';
 
@@ -22,15 +24,18 @@ function CheckersPage(props: CheckersPageProps) {
     )}`;
 
     const [selectedPiece, setSelectedPiece] = useState<[number, number]>();
-    const [selectedField, setSelectedField] = useState<[number, number]>();
     const [game, setGame] = useState<GameState>(INITIAL_STATE);
 
     const isFieldPlayable = (x: number, y: number) => (x % 2 === 0 ? y % 2 !== 0 : y % 2 === 0);
     const handlePieceOnCLick = (x: number, y: number) => () => setSelectedPiece([x, y]);
-    const handleFieldOnClick = (x: number, y: number) => () => setSelectedField([x, y]);
+    const handleFieldOnClick = (x: number, y: number) => () => {
+        const message = JSON.stringify({ move: [selectedPiece, [x, y]] });
+        const compressedMessage = gzip(encodeUtf8(message));
+        sendMessage(compressedMessage);
+    };
     async function handleGameMessage(event: WebSocketEventMap['message']) {
-        if (event?.data.error) {
-            console.log('server error', event.data.error);
+        if (typeof event.data === 'string') {
+            console.log('server error', event.data);
         } else {
             const message = await event.data.arrayBuffer();
             const obj = JSON.parse(decodeUtf8(ungzip(message)));
@@ -38,10 +43,21 @@ function CheckersPage(props: CheckersPageProps) {
             setGame(obj);
         }
     }
+    const websocketClosed = (state: ReadyState) =>
+        state === ReadyState.CLOSING || state === ReadyState.CLOSED;
     const getGameStatus = () => {
-        if (game.game_status === 'wait') return 'Waiting for the other player...';
-
-        return game.your_move ? 'Your turn!' : "Opponent's turn!";
+        switch (game.game_status) {
+            case GAME_STATUS.wait:
+                return 'Waiting for the other player...';
+            case GAME_STATUS.won:
+                return 'You won!';
+            case GAME_STATUS.lost:
+                return 'You lost!';
+            case GAME_STATUS.draw:
+                return "It's a draw!";
+            default:
+                return game.your_move ? 'Your turn!' : "Opponent's turn!";
+        }
     };
 
     const websocketOptions = {
@@ -50,6 +66,12 @@ function CheckersPage(props: CheckersPageProps) {
 
     //@ts-ignore
     const { sendMessage, readyState } = useWebSocket(websocketUrl, websocketOptions);
+
+    useEffect(() => {
+        if (!game.your_move) setSelectedPiece(undefined);
+    }, [game.your_move]);
+
+    if (websocketClosed(readyState)) return <h1>Connection lost!</h1>;
 
     return (
         <Container>
